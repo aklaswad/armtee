3b1882d79ad2a49f39cf8012cc3056820c37e929
Akira Sawada <aklaswad@gmail.com>
2023-02-20 08:21:48 +0900

    fix(site): Builder needs plain replace...

--- site/build.js
+++ site/build.js
@@ -1,12 +1,14 @@
 import MD from 'markdown-it'
 import toc from "markdown-it-table-of-contents"
 import anchor from "markdown-it-anchor"
+import fs from 'fs'
+
 const md = new MD()
 md.use(anchor, {
+  slugify: (s) => 'doc-' + encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-')),
   permalink: anchor.permalink.headerLink()
 })
 md.use(toc)
-import fs from 'fs'
 const doc = fs.readFileSync('./site/doc.md', 'utf-8')
 let tocHtml
 const rendered = md.render(doc).replace( /<p>TOCBEGIN(.*)TOCEND<\/p>/s, (match, p1) => {
@@ -14,9 +16,21 @@ const rendered = md.render(doc).replace( /<p>TOCBEGIN(.*)TOCEND<\/p>/s, (match, 
   return ''
 })
 
-
+// Poor replace. I need this since JS String.replace() will
+// *ALWAYS* try expanding RegExp replcement patterns like `$1`
+// though matcher is not regexp.
+function simpleReplace (src, pattern, replacement) {
+  const parts = src.split(pattern)
+  const before = parts.shift();
+  return before + replacement + parts.join('')
+}
 const htmlSrc = fs.readFileSync('./site/index.html', 'utf-8')
-const html = htmlSrc.replace('<!--CONTENT_PLACEHOLDER-->', rendered).replace('<!--TOC_PLACEHOLDER-->', tocHtml)
-
+const html = simpleReplace(
+  simpleReplace(
+    htmlSrc,
+    '<!--CONTENT_PLACEHOLDER-->',
+    rendered),
+  '<!--TOC_PLACEHOLDER-->',
+  tocHtml)
 fs.writeFileSync('./index.html', html)
 
