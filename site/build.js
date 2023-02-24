355d586e75aedd3ed825d3c0eae6f902b54c614d
Akira Sawada <aklaswad@gmail.com>
2023-02-25 06:07:56 +0900

    feat(site): Introduced markdown-it-attrs
    ```
    $ npm i -D markdown-it-attrs
    ```

--- site/build.js
+++ site/build.js
@@ -1,6 +1,7 @@
 import MD from 'markdown-it'
 import toc from "markdown-it-table-of-contents"
 import anchor from "markdown-it-anchor"
+import attrs from 'markdown-it-attrs'
 import fs from 'fs'
 
 const md = new MD()
@@ -15,6 +16,12 @@ md.use(anchor, {
 md.use(toc, {
   includeLevel: [1,2,3],
 })
+md.use(attrs, {
+  // optional, these are default options
+  leftDelimiter: '{',
+  rightDelimiter: '}',
+  allowedAttributes: []  // empty array = all attributes are allowed
+});
 const doc = fs.readFileSync('./site/doc.md', 'utf-8')
 let tocHtml
 const rendered = md.render(doc).replace( /<p>TOCBEGIN(.*)TOCEND<\/p>/s, (match, p1) => {
