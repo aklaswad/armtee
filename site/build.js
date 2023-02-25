8d138a88de3418a1a77d9f8fdbebbcd320144666
Akira Sawada <aklaswad@gmail.com>
2023-02-26 07:45:59 +0900

    fix(site): Save white space in the `inline code`

--- site/build.js
+++ site/build.js
@@ -1,4 +1,5 @@
 import MD from 'markdown-it'
+import {escapeHtml} from 'markdown-it/lib/common/utils.js'
 import toc from "markdown-it-table-of-contents"
 import anchor from "markdown-it-anchor"
 import attrs from 'markdown-it-attrs'
@@ -22,6 +23,18 @@ md.use(attrs, {
   rightDelimiter: '}',
   allowedAttributes: []  // empty array = all attributes are allowed
 });
+
+// Save white space in Inline code
+md.use( md => {
+  md.renderer.rules.code_inline = function (tokens, idx, options, env, slf) {
+  var token = tokens[idx];
+
+  return  '<code' + slf.renderAttrs(token) + '>' +
+          escapeHtml(token.content).replaceAll(' ', '&nbsp;') +
+          '</code>';
+  }
+})
+
 
 const doc = fs.readFileSync('./site/doc.md', 'utf-8')
 let tocHtml
