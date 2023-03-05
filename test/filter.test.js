6808651bf87bbb0ecbc9b849c31a5748c28414c9
Akira Sawada <aklaswad@gmail.com>
2023-03-05 19:44:36 +0900

    test: fix typo

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,4 +1,4 @@
-import { ArmteeRunner } from '../dist/Runner.js'
+import { ArmteeRunner } from '../dist/runner.js'
 import { testFromYaml } from './util.js'
 ArmteeRunner.addFilter( 'upper', str => str.toUpperCase() )
 ArmteeRunner.addFilter( 'lower', str => str.toLowerCase() )
