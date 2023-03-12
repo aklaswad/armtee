a9fa345fd361eb77d76185ec0e1c93d27d10482a
Akira Sawada <aklaswad@gmail.com>
2023-03-12 19:19:12 +0900

    test: Add escape test
    Now this is failing since I moved filter functionality into
    Tagged template literal, so old escape system for '`' and '${`
    has also modifyed by filters. Arrrggggghhh.

--- test/filter.test.js
+++ test/filter.test.js
@@ -3,6 +3,7 @@ import { testFromYaml } from './util.js'
 await testFromYaml('filter.data.yml', ArmteeRunner, {
   filters: {
     upper: str => str.toUpperCase(),
-    lower: str => str.toLowerCase()
+    lower: str => str.toLowerCase(),
+    void: str => ''
   }
 })
