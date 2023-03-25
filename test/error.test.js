9f2a714b25bf882920e4b6b2cde68e250913a7d6
Akira Sawada <aklaswad@gmail.com>
2023-03-25 17:00:09 +0900

    test: Add more error tests

--- test/error.test.js
+++ test/error.test.js
@@ -1,4 +1,4 @@
-import { ATError } from '../src/error'
+import { ATError, ATUnexpectedError } from '../src/error'
 
 describe('ATError', () => {
   test('create', () => {
@@ -9,12 +9,29 @@ describe('ATError', () => {
   test('create with history', () => {
     const error = new ATError(
       'foo', [
-        { type: 'file', file: 'foo.tmpl', line: 3 },
+        { type: 'file' },
         { type: 'file', file: 'bar.tmpl', line: 2 }
     ])
     expect("" + error).toBe(`foo
 From template file bar.tmpl line 2
+From template file _unknown_`)
+  })
+
+  test('ATError can append history', () => {
+    const error = new ATError(
+      'foo', [
+        { type: 'file', file: 'foo.tmpl', line: 3 },
+        { type: 'file', file: 'bar.tmpl', line: 2 }
+    ])
+    error.appendHistory({type: 'file', file: 'fizz.tmpl', line: 1})
+    expect("" + error).toBe(`foo
+From template file fizz.tmpl line 1
+From template file bar.tmpl line 2
 From template file foo.tmpl line 3`)
   })
 
+  test('ATUnexpectedError', () => {
+    const error = new ATUnexpectedError('foo')
+    expect("" + error).toMatch(/Unsupported type: foo/)
+  })
 })
